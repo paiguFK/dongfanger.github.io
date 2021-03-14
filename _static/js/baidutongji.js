847a3d491fda6a38f11c5597f2bbaf684a0019e5
dongfanger <dongfanger@outlook.com>
2021-03-14 23:04:20 +0800

    blog

--- _static/js/baidutongji.js
+++ _static/js/baidutongji.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?f5c255b86f0fdeda8d508737a6a1de97";
+  hm.src = "https://hm.baidu.com/hm.js?f51076579950b5499225970b2a469edd";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
