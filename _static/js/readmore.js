3f2d6b39b3b84778ef1ff5685a7177cb885f5b4f
dongfanger <dongfanger@outlook.com>
2021-06-17 21:38:48 +0800

    github

--- _static/js/readmore.js
+++ _static/js/readmore.js
@@ -39,7 +39,7 @@ var setIdTimer = setInterval(function () {
             btw.init({
                 id: id,
                 blogId: '26021-1615627409260-510',
-                name: 'dongfanger或Java代码美学',
+                name: 'Java代码美学或dongfanger',
                 qrcode: '../_images/vip.jpg',
                 keyword: '电子书'
             });
